7d16333311e4f695837b59d64e62152867ed2824
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2023-12-28 14:19:11 +0200

    Format development files

--- src/main/java/com/mp/aspectcalculator/AspectApp.java
+++ src/main/java/com/mp/aspectcalculator/AspectApp.java
@@ -7,17 +7,21 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class AspectApp extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        FXMLLoader sceneFile = new FXMLLoader(AspectApp.class.getResource("aspect-view.fxml"));
+        Scene scene = new Scene(sceneFile.load(), 320, 240);
+
+        stage.setTitle("Aspect Calculator");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+
         launch();
     }
 }
